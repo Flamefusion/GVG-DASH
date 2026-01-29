b4a12a4e10bad759270d4f4924ecda50b320bffe
Flamefusion <meshekharbehera613@gmail.com>
2026-01-30 01:02:42 +0530

    Schema: updating the schema of the table to proper standard

--- bigquery_schema_update.sql
+++ bigquery_schema_update.sql
@@ -1,12 +1,13 @@
 CREATE OR REPLACE TABLE `production-dashboard-482014.dashboard_data.master_station_data`
 (
-    vqc_inward_date STRING,
+    vqc_inward_date DATE,
     serial_number STRING NOT NULL,
     vqc_status STRING,
     vqc_reason STRING,
-    ft_inward_date STRING,
+    ft_inward_date DATE,
     ft_status STRING,
     ft_reason STRING,
+    cs_comp_date DATE,
     cs_status STRING,
     cs_reason STRING,
     size STRING,
