7bb3f20a4b2b43921d5f17489e60e7a4ed9ed748
Flamefusion <meshekharbehera613@gmail.com>
2026-02-14 23:20:39 +0530

    WIP: created a optimized summary table for wip calcualtion  chart

--- backend/cloud_functions/bq_trigger/main.py
+++ backend/cloud_functions/bq_trigger/main.py
@@ -14,6 +14,7 @@ def bq_trigger_handler(event, context):
     try:
         update_dash_overview()
         update_rejection_analysis()
+        update_wip_sku_wise()
         print("Successfully updated all live summary tables.")
     except Exception as e:
         print(f"Error during update: {e}")
@@ -112,6 +113,31 @@ def update_dash_overview():
         SAFE_DIVIDE(COUNTIF(cs_status = 'ACCEPTED'), COUNT(*)) AS yield
 
     FROM single_scan_funnel
+    GROUP BY 1, 2, 3, 4, 5, 6;
+    """
+    query_job = client.query(sql)
+    query_job.result()
+
+def update_wip_sku_wise():
+    sql = """
+    CREATE OR REPLACE TABLE `production-dashboard-482014.dashboard_data.wip_sku_wise` AS
+    SELECT
+        vqc_inward_date AS event_date,
+        line,
+        CASE 
+            WHEN ft_inward_date IS NULL THEN 'VQC'
+            ELSE 'FT'
+        END AS stage,
+        sku,
+        size,
+        vendor,
+        COUNT(*) AS wip_count
+    FROM `production-dashboard-482014.dashboard_data.master_station_data`
+    WHERE vqc_inward_date IS NOT NULL
+    AND (UPPER(vqc_status) NOT IN ('SCRAP', 'WABI SABI', 'RT CONVERSION') OR vqc_status IS NULL)
+    AND (UPPER(ft_status) NOT IN ('REJECTED', 'AESTHETIC SCRAP', 'FUNCTIONAL BUT REJECTED', 'SCRAP', 'SHELL RELATED', 'WABI SABI', 'FUNCTIONAL REJECTION') OR ft_status IS NULL)
+    AND (UPPER(cs_status) != 'REJECTED' OR cs_status IS NULL)
+    AND (cs_status != 'ACCEPTED' OR cs_status IS NULL)
     GROUP BY 1, 2, 3, 4, 5, 6;
     """
     query_job = client.query(sql)
