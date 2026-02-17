ba13a1724b397d27f69bb3ec80f440bb0a0414b8
Flamefusion <meshekharbehera613@gmail.com>
2026-02-18 05:08:04 +0530

    user: user specific initials and colors

--- backend/users.sql
+++ backend/users.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS `production-dashboard-482014.dashboard_data.users` (
   email STRING NOT NULL,
   password_hash STRING NOT NULL,
   role STRING,
+  init STRING,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 );
